c63a676a344e79cc07e6d0832ed5dbc42c57ea46
po_miyasaka <miyasaka@yahoo.co.jp>
2020-02-01 00:45:07 +0900

    fix indent

--- tarou.js
+++ tarou.js
@@ -3,9 +3,9 @@ javascript:
 // consoleを出してコメントのURLを受け付ける
 
 // ループ
-    // More Loadの配列を作る
-    // More Loadを展開
-    // 
+    // More Loadｓを作る
+        // More Loadを展開
+        // 
     // 少し待つ。
 
 // コメントまで飛ばす
