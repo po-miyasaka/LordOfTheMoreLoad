2e1e54ea437f122e7322180f972d647e936d0f3d
po_miyasaka <miyasaka@yahoo.co.jp>
2020-02-01 22:37:59 +0900

    fix indent

--- tarou.js
+++ tarou.js
@@ -2,14 +2,15 @@ javascript:
 let url = prompt("任意のコメントURL");
 
 function open() {
-  let moreLoads = document.querySelectorAll('form.pagination-loader-container');
-  moreLoads.forEach(f => f.querySelector('[type="submit"]').click());
-  return moreLoads.length > 0
+	let moreLoads = document.querySelectorAll('form.pagination-loader-container');
+	moreLoads.forEach(f => f.querySelector('[type="submit"]').click());
+	return moreLoads.length > 0
 };
 
-while (true) {let didMoreLoad = open();
-if (didMoreLoad)break ;
-sleep(100);
+while (true) {
+	let didMoreLoad = open();
+	if (didMoreLoad)break ;
+	sleep(1000);
 };
 
 if (url)document.location.href = url;
