19e92226feebde9cd994c6f4e2acafea09a1ac1e
po_miyasaka <miyasaka@yahoo.co.jp>
2020-02-02 17:18:31 +0900

    マトリョーシカ対応の修正

--- tarou.js
+++ tarou.js
@@ -3,15 +3,15 @@ javascript:
 function open() {
 	let moreLoads = document.querySelectorAll('form.pagination-loader-container');
 	moreLoads.forEach(f => f.querySelector('[type="submit"]').click());
-	return moreLoads.length > 0
+	return moreLoads.length == 0
 };
 
 function process() {
 	let url = prompt("任意のコメントURL");
 	while (true) {
-		let didMoreLoad = open();
-		if (didMoreLoad)break ;
-		sleep(1000);
+		let noMoreLoad = open();
+		if (noMoreLoad) break ;
+		sleep(3000);
 	};
 	if (url) window.location.href = url;
 };
