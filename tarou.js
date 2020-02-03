036ba87905e9cde16276c4c872cb8c064a759344
Kazutoshi Miyasaka <miyasaka_kazutoshi@yahoo.co.jp>
2020-02-04 06:59:04 +0900

    いらんログ削除

--- tarou.js
+++ tarou.js
@@ -3,7 +3,6 @@ javascript:
 function openAllPagination() {
 	return new Promise((resolve) => {
 		let pagination = () => {
-			console.log("pagination");
 			let moreLoads = document.querySelectorAll('form.pagination-loader-container');
 			if (moreLoads.length == 0){
 				clearInterval(paginationID); 
